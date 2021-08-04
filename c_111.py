87ea0eda545756a85e521fde12a6f74e2915032e
Snigdha_13 <65667637+Snigdha-Pothuri@users.noreply.github.com>
2021-08-04 21:22:25 +0530

    Add files via upload

--- c_111.py
+++ c_111.py
@@ -1,9 +1,11 @@
 import statistics
 import plotly.graph_objects as go 
 import plotly.figure_factory as ff 
-import pandas as pd 
+import pandas as pd  
+import random 
+mean=0
 df=pd.read_csv("111.csv")
-population_mean=statistics.mean(df)
+data = df["reading_time"].tolist()
 def random_set_of_mean(counter):
     dataset=[]
     for i in range(0,counter):
@@ -11,17 +13,18 @@ def random_set_of_mean(counter):
         value=data[random_index]
         dataset.append(value)
     mean=statistics.mean(dataset)
-    return mean
+    return mean 
+def plot_graph(mean_list): 
+    df=mean_list 
+    figure=ff.create_distplot([df],["reading_time"],show_hist=False) 
+    figure.show()  
+mean_list=[]
 def setUp():
-    mean_list=[]
     for i in range(0,100):
         set_of_means=random_set_of_mean(30)
         mean_list.append(set_of_means)
-    plot_graph(mean_list)
-def plot_graph():
-    df=mean_list
-    figure=ff.create_distplot([df],["temp"],show_hist=False)
-    figure.show() 
+    plot_graph(mean_list) 
+setUp()
 std=statistics.stdev(mean_list)
 mean1=statistics.mean(mean_list)
 first_std_start,first_std_end=mean1-std,mean1+std
@@ -35,4 +38,6 @@ fig.add_trace(go.scatter(x=[second_std_start,second_std_start],y=[0,0.17],mode="
 fig.add_trace(go.scatter(x=[second_std_end,second_std_end],y=[0,0.17],mode="lines",name="STANDARD DEVIATION 2 END"))
 fig.add_trace(go.scatter(x=[third_std_start,third_std_start],y=[0,0.17],mode="lines",name="STANDARD DEVIATION 3 START"))
 fig.add_trace(go.scatter(x=[third_std_end,third_std_end],y=[0,0.17],mode="lines",name="STANDARD DEVIATION 3 END"))
-fig.show()
+fig.show()  
+z_score=(mean1 - mean)/std 
+print("The z-score of this data is",z_score)
